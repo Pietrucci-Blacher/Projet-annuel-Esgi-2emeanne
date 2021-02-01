4610d9e0986ade34df20dbf969c0c11d7182c2b5
Pierre <bonnamy.77@gmail.com>
2021-02-01 14:15:29 +0100

    modify bdd access

--- testCurl.php
+++ testCurl.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=testcurl;charset=utf8','root','root');
+$pdo = new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_a4b01b2a0b88f60','bdd1b420797f42','190eb870');
 
 $text = $_POST['text'];
 
