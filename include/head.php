858c16aad49a5c74463225dae91e6efe49fa4473
Pierre <bonnamy.77@gmail.com>
2021-02-02 20:44:55 +0100

    add bootstrap,logo asset

---
+++ include/head.php
@@ -0,0 +1,4 @@
+<meta charset="utf-8">
+<title>Ultimate Parcel</title>
+<link rel="icon" type="image/png" href="asset/baseLogo.png" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
