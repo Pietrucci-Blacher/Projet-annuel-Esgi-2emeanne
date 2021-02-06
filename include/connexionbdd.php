1ebb7318f4bfba8504856e6b64cbd046d2ce755d
Sunshio <maxime.pietrucci@gmail.com>
2021-02-06 10:03:00 +0100

    Update PDO for connection return

--- include/connexionbdd.php
+++ include/connexionbdd.php
@@ -1,12 +1,13 @@
 <?php
-require ('include/variable.php');
+require_once('include/variable.php');
 
 function connexionBDD(){
 	try{
 		$bdd = new PDO(driver.":host=".host.";dbname=".dbname,user,pwd, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+		return $bdd;
 	}catch(Exception $e){
 		die('Erreur : ' . $e->getMessage());
 	}
-	return $bdd;
+
 }
 ?>
