cfca7978401f5d8674e510464d7bf1838e398209
Sunshio <maxime.pietrucci@gmail.com>
2021-02-22 19:15:37 +0100

    Fix file

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <!Doctype html>
 <html lang="fr" dir="ltr">
-<head> 
+<head>
 <?php require_once('include/head.php'); ?>
     <?php require_once('include/script.php'); ?>
     <meta name="description" content="Page de connexion de l'application web Ultimate Parcer">
@@ -21,17 +21,17 @@
           <div class="card-body bg-light">
             <h5 class="card-title text-center mb-3">Se Connecter</h5>
             <form class="col-md-12" id="formconnectcheck" action="include/signincheck.php" method="POST" enctype="multipart/form-data">
-            <div class="form-group">
+            <div class="form-group mb-2">
                 <input name="email" class="form-control" type="email" placeholder="E-Mail" autofocus><br>
             </div>
-            <div class="form-group">
+            <div class="form-group mb-2">
                 <input id="password" name="password" class="form-control" type="password" placeholder="Mot de passe"><br>
             </div>
             <div class="custom-control custom-checkbox mb-4">
                 <input type="checkbox" class="custom-control-input" id="showpassword">
                 <label class="custom-control-label" for="customCheck1">Afficher le mot de passe</label>
             </div>
-            <br>
+            <hr>
             <div class="custom-control custom-checkbox mb-4">
               <input name="remember" type="checkbox" class="custom-control-input" id="customCheck1">
               <label class="custom-control-label" for="customCheck1">Se souvenir de moi</label>
