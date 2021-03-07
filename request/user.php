32c397be8999d4cf68d208c3c3f848d1a8e04cca
Sunshio <maxime.pietrucci@gmail.com>
2021-03-07 16:15:03 +0100

    Fix bug to print datas

--- request/user.php
+++ request/user.php
@@ -4,13 +4,10 @@ require_once __DIR__ . '/../include/connexionbdd.php';
 
 function getData(){
     $bdd = connexionBDD();
-
-
-    $res = $bdd -> prepare("SELECT genre, nom, prenom, motdepasse, status, adresse, ville, codePostal, email, numPhone FROM client WHERE id = :id");
+    $res = $bdd -> prepare("SELECT genre, nom, prenom, mdp, status, adresse, ville, codePostal, email, numPhone FROM client WHERE id = :id");
     $res->bindValue(":id", $_SESSION['id'], PDO::PARAM_INT);
     $res->execute();
     $uinfo = $res->fetchAll(PDO::FETCH_ASSOC);
-
     return $uinfo;
 
 }
