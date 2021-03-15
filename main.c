3df0b73520f293cd82af82d9d949e5d14d5432af
Pierre <bonnamy.77@gmail.com>
2021-03-15 15:51:13 +0100

    init commit (with mysql lib)

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <winsock.h>
+#include <mysql.h>
+
+int main(int argc,char **argv)
+{
+    printf("Hello world!\n");
+    return 0;
+}
