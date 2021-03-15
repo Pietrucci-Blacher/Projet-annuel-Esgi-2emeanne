1f98bb09cbeec1164163a022e09ec00c97595eab
Pierre <bonnamy.77@gmail.com>
2021-03-15 16:31:18 +0100

    connection with heroku db

--- main.c
+++ main.c
@@ -4,8 +4,22 @@
 #include <winsock.h>
 #include <mysql.h>
 
+MYSQL mysql;
+
+int mysql_connection(){
+    mysql_init(&mysql);
+    if (!mysql_real_connect(&mysql, "eu-cdbr-west-03.cleardb.net", "bdd1b420797f42", "190eb870", "heroku_a4b01b2a0b88f60", 3306, NULL, 0)){
+        // error
+        printf("MYSQL CONNECT ERROR");
+        return -1;
+    } else {
+        printf("MYSQL CONNECT SUCCESS");
+        return 0;
+    }
+}
+
 int main(int argc,char **argv)
 {
-    printf("Hello world!\n");
+    mysql_connection();
     return 0;
 }
