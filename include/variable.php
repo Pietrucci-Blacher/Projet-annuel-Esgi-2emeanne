6beb31c03ffb657e126ee584fa0113db18050246
Sunshio <maxime.pietrucci@gmail.com>
2021-03-18 00:55:08 +0100

    Add new variables for file support

--- include/variable.php
+++ include/variable.php
@@ -4,4 +4,10 @@ define("dbname", "heroku_a4b01b2a0b88f60");
 define("user","bdd1b420797f42");
 define("pwd","190eb870");
 define("driver","mysql");
-?>
+
+define('SITE_ROOT', realpath(dirname(dirname(__FILE__))));
+
+define('KB', 1024);
+define('MB', 1048576);
+define('GB', 1073741824);
+define('TB', 1099511627776);
