ba3b484c60221b7bc4a5fd7361627c3a443edc9a
Sunshio <maxime.pietrucci@gmail.com>
2021-03-18 15:27:13 +0100

    Update signincheck.php

--- include/signincheck.php
+++ include/signincheck.php
@@ -1,12 +1,13 @@
 <?php
-session_start(); 
+require_once('../request/user.php'); 
 require_once(__DIR__.'/connexionbdd.php');
+
 $bdd = connexionBDD(); 
 
 $email = isset($_POST['email']) ? $_POST['email'] : '';
 $password = isset($_POST['password']) ? sha1(htmlspecialchars($_POST['password'])) : '';
-$remember = $_POST['remember']; 
- 
+$remember = $_POST['remember'];
+
 $req = $bdd->prepare('SELECT id,nom,email,mdp,status FROM client WHERE email = :mail AND mdp = :mdp');
 $req->bindValue(":mail",$email,PDO::PARAM_STR); 
 $req->bindValue(":mdp",$password,PDO::PARAM_STR); 
@@ -14,8 +15,8 @@ $req->execute();
 if($data = $req->fetch()){
 	$_SESSION['rank'] = $data['status']; 
 	$_SESSION['email'] = $data['email'];
-	$_SESSION['name'] = $data['nom'];
 	$_SESSION['id'] = $data['id']; 
+	$_SESSION['name'] = $data['nom']; 
 	setcookie("rank",$data['status'],time() + 1814400); 
 	if(!empty($_POST["remember"])) {
 		setcookie ("email",$_POST['email'],time() + 31*24*3600, null, null, true);
@@ -26,8 +27,13 @@ if($data = $req->fetch()){
 		setcookie("password","");
 		setcookie("rank", ""); 
 	}
-	header('location: ../index.php');
-    exit(); 
+	if($data['status'] == "livreur" && checkfirstconnect() == true){
+	    header('Location: ../deliveryform.php');
+        exit();
+    }else{
+        header('location: ../index.php');
+        exit();
+    }
 }else{
 	header('location: ../connect.php');
     exit(); 
