0f13abffd9eb44c369df72e80347b98bbae75977
Paillusson Célian <72802385+PaillussonCelian@users.noreply.github.com>
2021-03-20 15:58:46 +0100

    modifprofilupdate

--- include/modifprof.php
+++ include/modifprof.php
@@ -12,13 +12,13 @@ $cdPostal = htmlspecialchars(trim($_POST['cdPostal']));
 $nbPhone = htmlspecialchars(trim($_POST['nbPhone']));
 
 
-/*$q = "SELECT nom, prenom, email, adresse, codePostal, numPhone FROM client WHERE id = :id";
+$q = "SELECT nom, prenom, email, adresse, codePostal, numPhone FROM client WHERE id = :id";
 $req = $bdd->prepare($q);
 $req->bindValue(":id", $_SESSION['id'], PDO::PARAM_INT);
 $req->execute();
-$checkempty= $req->fetchAll(PDO::FETCH_ASSOC);*/
+$checkempty= $req->fetchAll(PDO::FETCH_ASSOC);
 
-//if($checkempty != 0){
+if($checkempty != 0){
     if(isset($_POST['lastname']) && strlen($_POST['lastname']) >= 1 && strlen($_POST['lastname']) <= 70 && is_string($_POST['lastname'])){
         if(isset($_POST['firstname']) && strlen($_POST['firstname']) >= 1 && strlen($_POST['firstname']) <= 60 && is_string($_POST['firstname'])) {
             if (isset($_POST['address']) && strlen($_POST['address']) > 1 && is_string($_POST['address'])) {
@@ -40,4 +40,4 @@ $checkempty= $req->fetchAll(PDO::FETCH_ASSOC);*/
             }
         }
     }
-//}
+}
