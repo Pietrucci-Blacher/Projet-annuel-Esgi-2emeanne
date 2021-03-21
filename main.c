085774b08ee0e40d6b2105d07ad1a88101816433
Pierre <bonnamy.77@gmail.com>
2021-03-21 22:00:36 +0100

    query to bdd + open whole directory

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,47 +8,140 @@
 MYSQL mysql;
 char infoParcel[11][255];
 
+void readExcel();
 
-int mysql_connection(){
+void mysql_connection(){
     mysql_init(&mysql);
     if (!mysql_real_connect(&mysql, "eu-cdbr-west-03.cleardb.net", "bdd1b420797f42", "190eb870", "heroku_a4b01b2a0b88f60", 3306, NULL, 0)){
         // error
         printf("MYSQL CONNECT ERROR");
-        return -1;
+        mysql_close(&mysql);
     } else {
         printf("MYSQL CONNECT SUCCESS");
-        return 0;
+        readExcel();
     }
 }
 
-void makeQuery(){
+void displayArray(){
+    printf("\n");
+    for (int i = 0; i<11; i++){
+        printf(" %s ", infoParcel[i]);
+    }
 }
 
+void emptyArray(){
+    for (int i = 0; i<11; i++){
+        memset(infoParcel[i],0,sizeof(infoParcel[i]));
+    }
+
+}
+
+void queryParcel(int idClient){
+    char idClientStr[50];
+    char query[1000];
+
+    sprintf(idClientStr, "%d", idClient);
+
+    strcpy(query,"INSERT INTO COLIS(entreprise,poids,modeLivraison,refQrcode,status,client) VALUES (");
+    for(int i = 0; i<3; i++){
+        strcat(query,"\"");
+        strcat(query,infoParcel[i]);
+        strcat(query,"\"");
+        strcat(query,",");
+    }
+
+    strcat(query,"\"");
+    strcat(query,infoParcel[10]);
+    strcat(query,"\"");
+    strcat(query,",");
+    strcat(query,"\"En attente du partenaire\"");
+    strcat(query,",");
+    strcat(query,idClientStr);
+    strcat(query,");");
+
+    printf("\n%s",query);
+    if (mysql_query(&mysql, query)) {
+        printf("\nINSERT ERROR");
+    }else{
+        printf("\nINSERT SUCCESS");
+    }
+}
+
+
+void queryClient(){
+    int userId;
+    char query[1000];
+    strcpy(query,"INSERT INTO CLIENT(nom,prenom,adresse,ville,codePostal,info,numPhone,status) VALUES (");
+    for(int i = 3; i<11;i++){
+        if(i == 10){
+            strcat(query,"\"client\"");
+            strcat(query,");");
+        }else{
+            strcat(query,"\"");
+            strcat(query,infoParcel[i]);
+            strcat(query,"\"");
+            strcat(query,",");
+        }
+    }
+
+    printf("\n%s",query);
+
+    if (mysql_query(&mysql, query)) {
+        printf("\nINSERT ERROR");
+    }else{
+        printf("\nINSERT SUCCESS");
+        userId = mysql_insert_id(&mysql);
+        queryParcel(userId);
+    }
+}
+
+
 void readExcel(){
+    DIR *d;
+    struct dirent *dir;
+    char path[255];
     FILE* file;
-    file = fopen("upload/new.csv","r");
+
+    strcpy(path,"upload/");
     char character;
     int col = 0;
     int i =0;
 
-    if(file){
-        while(feof(file)==0){
-            character = fgetc(file);
-            if(character=='\n'){
-                makeQuery();
-                i=0;
-                col=0;
-            }else if (character == ';'){
-                col +=1;
-                i=0;
-            }else{
-                infoParcel[col][i] = character;
-                //printf("%s\n",infoParcel[col]);
-                i+=1;
+    d = opendir("upload");
+    if(d){
+
+        while ((dir = readdir(d)) != NULL)
+        {
+            if(strcmp(dir->d_name,".") != 0 && strcmp(dir->d_name,"..") != 0){
+                //printf("-%s-",dir->d_name);
+                strcat(path,dir->d_name);
+                file = fopen(path,"r");
+                if(file){
+                    printf("file");
+                    while(feof(file)==0){
+                        character = fgetc(file);
+                        if(character=='\n'){
+                            //displayArray();
+                            queryClient();
+                            emptyArray();
+                            i=0;
+                            col=0;
+                        }else if (character == ';'){
+                            col +=1;
+                            i=0;
+                        }else{
+                            infoParcel[col][i] = character;
+                            //printf("%s\n",infoParcel[col]);
+                            i+=1;
+                        }
+                    }
+                    fclose(file);
+                }
             }
         }
+        closedir(d);
     }
-    fclose(file);
+    mysql_close(&mysql);
 }
 
 int main(int argc,char **argv)
