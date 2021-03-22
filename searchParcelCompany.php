1028dfe8057a9cd434d506f7fa21c442888d0bd8
Pierre <bonnamy.77@gmail.com>
2021-03-22 23:54:06 +0100

    start of company parcel search

--- searchParcelCompany.php
+++ searchParcelCompany.php
@@ -18,6 +18,38 @@
       <div class="input-group input-group-lg">
         <input id="name" type="text" class="form-control text-center mt-5" placeholder="Recherche">
       </div>
+
+      <div class="d-flex mt-4">
+        <div class="flex-fill mx-2">
+          <p class="h5 text-center mb-4 banner-item">Rechercher par :</p>
+          <select class="form-select" aria-label="Default select example">
+            <option selected>N'importe</option>
+            <option value="1">Nom</option>
+            <option value="2">CodePostal</option>
+            <option value="3">Référence</option>
+          </select>
+        </div>
+
+        <div class="flex-fill mx-2">
+          <p class="h5 text-center mb-4 banner-item">Mode de livraison :</p>
+          <select class="form-select" aria-label="Default select example">
+            <option selected>N'importe</option>
+            <option value="1">Standard</option>
+            <option value="2">Express</option>
+          </select>
+        </div>
+
+        <div class="flex-fill mx-2">
+          <p class="h5 text-center mb-4 banner-item">Status du paiement :</p>
+          <select class="form-select" aria-label="Default select example">
+            <option selected>N'importe</option>
+            <option value="1">Payé</option>
+            <option value="2">Paiement en attente</option>
+          </select>
+        </div>
+
+      </div>
+
       <div class="d-grid gap-2 mx-auto">
         <button id="submit" type="button" class="btn btn-primary mt-5 btn-lg"><a class="serviceslink">VALIDER</a></button>
       </div>
