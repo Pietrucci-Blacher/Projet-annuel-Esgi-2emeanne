6216326621e64c92f9e37ee4d522c6c29ba09b84
Sunshio <maxime.pietrucci@gmail.com>
2021-04-14 17:33:59 +0200

    Remove Wr3dJS

--- js/map.js
+++
@@ -1,7 +0,0 @@
-L.Wrld.map("map", "b4cb0c738223dfccb7447e68864dc4a6",{
-    center: [48.856614,	2.3522219],
-    zoom:14,
-    frameRateThrottleWhenIdleEnabled: true,
-    throttledTargetFrameIntervalMilliseconds: 500,
-    idleSecondsBeforeFrameRateThrottle: 15.0
-});
