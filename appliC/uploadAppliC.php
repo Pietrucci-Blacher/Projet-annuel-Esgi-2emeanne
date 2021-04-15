20254b5b3513a45be7776f23c2a4a446b37b5044
Pierre <bonnamy.77@gmail.com>
2021-04-15 23:13:31 +0200

    upload appli C

--- appliC/uploadAppliC.php
+++ appliC/uploadAppliC.php
@@ -4,15 +4,7 @@ $putdata = fopen("php://input", "r");
 if (isset($putdata)) {
   echo "JE SUIS LA";
 }
-/* Ouvre un fichier pour écriture */
-$fp = fopen($_GET['name'] , "w");
 
-/* Lecture des données, 1 Ko à la fois et écriture dans le fichier */
-while ($data = fread($putdata, 1024))
-fwrite($fp, $data);
-
-/* Fermeture du flux */
-fclose($fp);
 fclose($putdata);
 
 ?>
