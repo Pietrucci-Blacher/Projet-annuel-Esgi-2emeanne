2fe869a67f605da226d8fb745b2b1564ea469bae
Sunshio <maxime.pietrucci@gmail.com>
2021-05-01 10:18:02 +0200

    Update header to add more translations

--- include/header.php
+++ include/header.php
@@ -7,15 +7,15 @@
     <div class="collapse navbar-collapse " id="navbarSupportedContent">
       <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
         <li class="nav-item">
-          <a class="nav-link" aria-current="true" href="index.php">Accueil</a>
+          <a langtrad="HOME" class="nav-link" aria-current="true" href="index.php">Accueil</a>
         </li>
         <?php if(!isset($_SESSION['name'])){ ?>
         <li class="nav-item">
-          <a class="nav-link" href="parcelTracking.php">Suivre mon colis</a>
+          <a langtrad="FPACKAGE" class="nav-link" href="parcelTracking.php">Suivre mon colis</a>
         </li>
         <?php }  ?>
           <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a langtrad="PRICE" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Tarifs
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
