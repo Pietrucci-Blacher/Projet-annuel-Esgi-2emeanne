365bceb3a0cbe8ed1cd98431978dc1a94c0e253d
Sunshio <maxime.pietrucci@gmail.com>
2021-05-03 00:44:49 +0200

    Update deliveryform boostrap tags

--- deliveryform.php
+++ deliveryform.php
@@ -29,10 +29,13 @@ if(empty($_SESSION) || !$_SESSION['rank'] == "livreur" || checkfirstconnect() ==
 <div class="container-fluid">
     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
         <h3 class="text-center fw-bold mb-3" langtrad="ENR">Enregistrement livreurs</h3>
+        <br>
         <form class="border bg-light border-dark rounded text-align" action="include/deliverycheck.php" id="deliveryformcheck" method="post" enctype="multipart/form-data">
             <br>
             <div class="form-group flex-fill mx-3 mb-2" id="driveuploadzone">
+               <div class="text-center">
                 <label for="driveupload" class="fw-bold mb-2" langtrad="UPLO">Veuillez upload votre permis de conduire</label>
+               </div>   
                 <input class="form-control mb-2" type="file" name="driveupload" id="driveupload">
                 <blockquote class="blockquote">
                     <h5 langtrad="EXTA">Extension acceptée : PDF</h5>
@@ -40,7 +43,9 @@ if(empty($_SESSION) || !$_SESSION['rank'] == "livreur" || checkfirstconnect() ==
             </div>
             <hr class="mx-4">
             <div class="form-group flex-fill mx-4  mb-2">
+               <div class="text-center">
                 <label langtrad="DEPOSIT" for="depot" class="fw-bold mb-2">Veuillez choisir votre dépôt</label>
+               </div>   
                 <select class="form-select" id="depot" name="depot">
                     <?php
                     $depots = getdepots();
@@ -51,8 +56,10 @@ if(empty($_SESSION) || !$_SESSION['rank'] == "livreur" || checkfirstconnect() ==
             </div>
             <br>
             <div class="form-group flex-fill mx-4 mb-2">
+               <div class="text-center">
                 <label langtrad="RADDEP" for="radiusdepot" class="fw-bold mb-2">Veuillez sélectionner la distance maximale de livraison autour de votre dépôt</label>
-                <select class="form-select" id="radiusdepot" name="radiusdepot">
+               </div> 
+                  <select class="form-select" id="radiusdepot" name="radiusdepot">
                     <?php
                     for($i = 30; $i <=300; $i += 10){
                         echo "<option value='$i'>". $i . " km" ."</option>";
@@ -61,7 +68,9 @@ if(empty($_SESSION) || !$_SESSION['rank'] == "livreur" || checkfirstconnect() ==
             </div>
             <hr class="mx-4">
             <div class="form-group flex-fill mx-3 mb-2">
-                <label for="driveuploadpoints" class="fw-bold mb-2 " langtrad="ULPO2">Veuillez upload un justificat montrant votre nombre de points sur le permis</label>
+               <div class="text-center">
+                <label for="driveuploadpoints" class="fw-bold mb-2 " langtrad="UPLO2">Veuillez upload un justificat montrant votre nombre de points sur le permis</label>
+               </div> 
                 <input class="form-control" type="file" name="driveuploadpoints" id="driveuploadpoints">
                 <br>
                 <blockquote>
