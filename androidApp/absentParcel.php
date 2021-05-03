61bef665530b0083d9b1ce873f509f185312dc85
Pierre <bonnamy.77@gmail.com>
2021-05-03 11:18:02 +0200

    change date absent parcel

--- androidApp/absentParcel.php
+++ androidApp/absentParcel.php
@@ -8,13 +8,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$date = date('Y-m-d', strtotime("+1 day"));
+
 $parcelId = $_POST['parcelId'];
 $deliveryId = $_POST['deliveryId'];
 
 $query = $bdd->prepare("UPDATE contient SET status = 'Absent' WHERE colis = ? AND livraison = ?");
 $query->execute([$parcelId,$deliveryId]);
 
-$query = $bdd->prepare("UPDATE colis SET status = 'Retour au dépot' WHERE id = ?");
-$query->execute([$parcelId]);
+$query = $bdd->prepare("UPDATE colis SET status = 'Retour au dépot',date = ? WHERE id = ?");
+$query->execute([$parcelId,$date]);
 
  ?>
