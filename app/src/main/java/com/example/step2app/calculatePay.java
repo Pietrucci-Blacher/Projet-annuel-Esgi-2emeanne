17e77358f5224a56baf2fbf6b164587d1832fd44
Pierre <bonnamy.77@gmail.com>
2021-05-06 16:05:25 +0200

    pay deliverer with big decimal

--- app/src/main/java/com/example/step2app/calculatePay.java
+++ app/src/main/java/com/example/step2app/calculatePay.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class calculatePay extends AppCompatActivity {
 
     private TextView parcelPrice,kmPrice,weightPrime,objectifPrime,objectifPrimeDetail;
     private Button btnPay;
-    private double totalPay,totalWeightPrime;
+    private BigDecimal totalPay,totalWeightPrime,prime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,27 +52,51 @@ public class calculatePay extends AppCompatActivity {
         kmPrice.setText(nbKm.toString() + " km parcourus x 0.36 €");
         objectifPrimeDetail.setText(delivered.toString()+" colis livrés / "+affected.toString()+" colis affectés");
 
-        totalPay=0;
+        totalPay=new BigDecimal("0");
 
-        totalPay += delivered*1.90;
-        totalPay += nbKm*0.36;
+        totalPay=totalPay.add(new BigDecimal(delivered).multiply(new BigDecimal("1.90")));
+        totalPay=totalPay.add(new BigDecimal(nbKm).multiply(new BigDecimal("0.36")));
 
-        totalWeightPrime = 0;
+        totalWeightPrime = new BigDecimal("0");
 
         for (int i = 0; i < parcelSup30; i++){
             try {
                 JSONArray sup30 = jsonObj.getJSONArray("sup30");
-                totalWeightPrime+= Math.ceil(Integer.parseInt(sup30.getJSONObject(i).getString("weight"))/22)*3;
+                BigDecimal temp = new BigDecimal(sup30.getJSONObject(i).getString("weight"));
+                temp=temp.subtract(new BigDecimal("30"));
+                temp=temp.divide(new BigDecimal("22"),2,BigDecimal.ROUND_HALF_EVEN);
+                temp=temp.setScale(0,RoundingMode.CEILING);
+                temp=temp.multiply(new BigDecimal("3"));
+                totalWeightPrime=totalWeightPrime.add(temp);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
 
-        weightPrime.setText(String.valueOf(totalWeightPrime)+" € de prime charges lourdes");
+        weightPrime.setText(totalWeightPrime.toString()+" € de prime charges lourdes");
 
-        totalPay+=totalWeightPrime;
+        totalPay=totalPay.add(totalWeightPrime);
 
 
+        Integer percentPrime = (delivered*100)/affected;
+
+        prime = new BigDecimal("0");
+
+        if(percentPrime>87){
+            prime.add(new BigDecimal("10").multiply(totalPay).divide(new BigDecimal("100")));
+        }else if(percentPrime>72){
+            prime.add(new BigDecimal("120"));
+        }else if (percentPrime > 60){
+            prime.add(new BigDecimal("50"));
+        }else if (percentPrime <= 10){
+            prime.subtract(new BigDecimal("15").multiply(totalPay).divide(new BigDecimal("100")));
+        }
+
+        objectifPrime.setText(String.valueOf(prime)+" € de prime d'objectif");
+
+        totalPay=totalPay.add(prime);
+
+        btnPay.setText("Récupérer "+String.valueOf(totalPay)+" €");
     }
 
 }
