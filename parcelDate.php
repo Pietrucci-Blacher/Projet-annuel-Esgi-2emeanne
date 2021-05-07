a0e447e1c49b6f22967c21e4771446136064518e
Pierre <bonnamy.77@gmail.com>
2021-05-07 17:25:43 +0200

    change location

--- parcelDate.php
+++ parcelDate.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once('../include/connexionbdd.php');
+require_once('include/connexionbdd.php');
 
 $bdd = connexionBDD();
 
