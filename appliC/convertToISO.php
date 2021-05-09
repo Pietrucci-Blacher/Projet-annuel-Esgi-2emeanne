bf23e31e08f1ed984211c6d70336376d47d9648f
Pierre <bonnamy.77@gmail.com>
2021-05-09 10:50:39 +0200

    add apk

--- appliC/convertToISO.php
+++ appliC/convertToISO.php
@@ -12,12 +12,12 @@ $idArray = explode("_",$idList);
 
 $count = 0;
 for ($i=0; $i < count($idArray); $i++) {
-  $query = $bdd->prepare("SELECT adresse,nom,prenom FROM CLIENT WHERE id = ?");
+  $query = $bdd->prepare("SELECT adresse,nom,prenom,ville FROM CLIENT WHERE id = ?");
   $query->execute([$idArray[$count]]);
   $client = $query->fetch();
 
-  $query2 = $bdd->prepare("UPDATE CLIENT SET adresse = ?, nom = ?, prenom = ? WHERE id = ?");
-  $query2->execute([utf8_decode($client['adresse']),utf8_decode($client['nom']),utf8_decode($client['prenom']),$idArray[$count]]);
+  $query2 = $bdd->prepare("UPDATE CLIENT SET adresse = ?, nom = ?, prenom = ?,ville=? WHERE id = ?");
+  $query2->execute([utf8_decode($client['adresse']),utf8_decode($client['nom']),utf8_decode($client['prenom']),utf8_decode($client['ville']),$idArray[$count]]);
 
   $count+=1;
 }
